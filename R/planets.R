8ef0fa2dfe79d8dffaa2dff0b5e06149194522ae
Raghav Kumar <raghavku@usc.edu>
2017-06-02 15:31:49 +0200

    Added Jupiter's Galilean Moons to planet tab

--- R/planets.R
+++ R/planets.R
@@ -19,30 +19,14 @@ if (FALSE) {
   # Distances are in units of 1 000 000 km.
   #
   planets <- data.frame(
-    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto'),
-    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4),
-    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244)
+    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Io','Europa','Ganymede','Callisto'),
+    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4, .422000,	.671000,1.070000,	1.883000),
+    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244, 0.0041,0.0094,0.0011,0.0074)
   )
   #
   # Convert from km to AU.
   #
   planets <- transform(planets, distance = distance * 1e6 / 1.496e8)
 
-  devtools::use_data(planets, overwrite = TRUE)
-}
-#Ellipses for Jupiter's Galilean Moons
-if (FALSE) {
-  # Distances are in units of km.
-  #
-  planets <- data.frame(
-    name = c('Io','Europa','Ganymede','Callisto'),
-    distance = c(422,000,	671,000,1,070,000,	1,883,000),
-    eccentricity = c(0.0041,0.0094,0.0011,0.0074)
-  )
-  #
-  # Convert from km to AU.
-  #
-  planets <- transform(planets, distance = distance * 1 / 1.496e8)
-  
   devtools::use_data(planets, overwrite = TRUE)
 }
