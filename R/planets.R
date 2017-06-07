a0c428efc1edad1903e3fb3e27238a2d6228f117
Griffin Doyle <griffindoyle3@gmail.com>
2017-06-07 16:41:41 +0200

    Added Ceres to the planets data frame.

--- R/planets.R
+++ R/planets.R
@@ -19,9 +19,9 @@ if (FALSE) {
   # Distances are in units of 1 000 000 km.
   #
   planets <- data.frame(
-    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Eris', 'Haumea', 'Makemake', 'Sedna', 'Quaoar', 'Orcus'),
-    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4, 10139.893, 6465.321, 6838.867, 75816.201, 6488.508, 5899.093),
-    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244, 0.44068, 0.19126, 0.15586, 0.85491, 0.034704, 0.2190)
+    name = c('Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto', 'Eris', 'Haumea', 'Makemake', 'Sedna', 'Quaoar', 'Orcus', 'Ceres'),
+    distance = c(57.9, 108.2, 149.6, 227.9, 778.6, 1433.5, 2872.5, 4495.1, 5906.4, 10139.893, 6465.321, 6838.867, 75816.201, 6488.508, 5899.093, 414.0),
+    eccentricity = c(0.205, 0.007, 0.017, 0.094, 0.049, 0.057, 0.046, 0.011, 0.244, 0.44068, 0.19126, 0.15586, 0.85491, 0.034704, 0.2190, 0.0758)
   )
   #
   # Convert from km to AU.
