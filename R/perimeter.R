7a1907af45a8e830efff1e7f82c454f8b0da2fc8
Antonio Minondo <aminondo@nd.edu>
2017-06-08 09:34:24 +0200

    changes to perimeter function, now minor axis is calculated for ramajanujan estimation

--- R/perimeter.R
+++ R/perimeter.R
@@ -1,16 +1,16 @@
 #' Calculate the length of the perimeter of an ellipse.
 #'
 #' @param r Length of the semi-major axis.
-#' @param e The eccentricity, if approx is true, then this parameter is the semi-minor axis
+#' @param e The eccentricity, 
 #' @param approx whether user wants to calculate ramanujan approximation
 #' @return The length of the perimeter of the ellipse with specified semi-major axis and eccentricity.
 #' @examples
 #' ellipse_perimeter(1, 1)
 #' ellipse_perimeter(1)
-#' ellipse_perimeter(10,5,approx=T)
+#' ellipse_perimeter(10,1,approx=T)
 #' @export
 ellipse_perimeter <- function(r, e = 0, approx=FALSE) {
-  if ((e < 0 || e > 1) && approx==F ) stop("ERROR: Invalid eccentricity!", call. = FALSE)
+  if ((e < 0 || e > 1)) stop("ERROR: Invalid eccentricity!", call. = FALSE)
   if(approx==F){
     if (e == 0) {
       2 * pi * r
@@ -18,8 +18,9 @@ ellipse_perimeter <- function(r, e = 0, approx=FALSE) {
       return(4 * r * pracma::ellipke(e)$e)
     }
   } else {
-    if(e>r) stop("ERROR: e is semi-minor axis and therefore needs to be smaller than r", call=F)
-    inside_func = 3*(r+e) - sqrt((3*r+e)*(r+3*e))
+    #calculate semi minor axis
+    b = r*sqrt(1-e^2)
+    inside_func = 3*(r+b) - sqrt((3*r+b)*(r+3*b))
     return( pi*inside_func)
   }
 }
